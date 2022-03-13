869e19e3899c6aa43f584889f0265ffbaf2612b2
Adam Decarte <adam.decarte92@gmail.com>
2022-03-13 23:56:49 +0100

    Added sendMoney test

--- src/test/java/com/pay_my_buddy/PayMyBuddy/services/TransactionServiceTest.java
+++ src/test/java/com/pay_my_buddy/PayMyBuddy/services/TransactionServiceTest.java
@@ -1,62 +1,35 @@
 package com.pay_my_buddy.PayMyBuddy.services;
 
 import com.pay_my_buddy.PayMyBuddy.data.BankAccountDAO;
+import com.pay_my_buddy.PayMyBuddy.data.BankAccountRepository;
 import com.pay_my_buddy.PayMyBuddy.data.UserRepository;
 import com.pay_my_buddy.PayMyBuddy.exception.BankTransactionException;
-import com.pay_my_buddy.PayMyBuddy.model.BankAccount;
 import com.pay_my_buddy.PayMyBuddy.model.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
-@ContextConfiguration(classes = TransactionServiceTest.class)
-//@TestPropertySource(locations = "classpath:application-test.properties")
 public class TransactionServiceTest {
-
-
     @Autowired
     UserRepository userRepository;
-
+    @Autowired
+    BankAccountRepository bankAccountRepository;
     @Autowired
     BankAccountDAO bankAccountDAO;
-
     @Test
     public void sendMoney() throws BankTransactionException {
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.setBankAccountNumber("FR86000000");
-
-        BankAccount bankAccountTo = new BankAccount();
-        bankAccountTo.setBankAccountNumber("FR93111111");
-
-        User user = new User();
-        user.setId(1);
-        user.setFirstname("Adam");
-        user.setLastname("Decarte");
-        user.setAccount(bankAccount);
-
-        User connection = new User();
-        connection.setId(2);
-        connection.setFirstname("John");
-        connection.setLastname("Doe");
-        connection.setAccount(bankAccountTo);
-
-
-        this.bankAccountDAO.sendMoney(user, connection, "", 300);
-
-        Optional<User> user1 = this.userRepository.findById(2);
-
-        assertEquals(connection, user1.get());
-
-
+        Optional<User> user1 = userRepository.findById(1);
+        Optional<User> user2 = userRepository.findById(2);
+        this.bankAccountDAO.sendMoney(user1.get(), user2.get(), "", 300);
+        Optional<User> userAssert = this.userRepository.findById(2);
+        assertEquals(1300, userAssert.get().getAccount().getBalance());
     }
-
 }
--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -1,2 +1,4 @@
-
-INSERT INTO `user` VALUES (1,'adam@test.com','Adam','Decarte','$2a$12$1q6ow9zAKusaqKwDKjWY0OwX5e07I.4BNbQkZWVSfY5vpxJo2BKae','user','adam@test.com',1,NULL,NULL),(2,'theo@test.com','Theo','Decarte','$2a$12$pWsVMMvtGhO5CUDDDRUNBuNtCrKVKdEOJV5LODmuMcE.efkY82tJe','user','theo@test.com',2,NULL,NULL),(3,'julien@test.com','Julien','Cauwet',NULL,'user','julien@test.com',NULL,NULL,NULL),(4,'tom@test.com','Tom','Decarte',NULL,'user',NULL,NULL,NULL,NULL);
+INSERT INTO bank_account VALUES (1, 1000, 'FR86000000', 'Adam', 'Decarte')
+INSERT INTO bank_account VALUES (2, 1000, 'FR86000001', 'Theo', 'Decarte')
+INSERT INTO user VALUES (1,'adam@test.com','Adam','Decarte','$2a$12$1q6ow9zAKusaqKwDKjWY0OwX5e07I.4BNbQkZWVSfY5vpxJo2BKae','user','adam@test.com',1)
+INSERT INTO user VALUES (2,'theo@test.com','Theo','Decarte','$2a$12$1q6ow9zAKusaqKwDKjWY0OwX5e07I.4BNbQkZWVSfY5vpxJo2BKae','user','theo@test.com',2)
